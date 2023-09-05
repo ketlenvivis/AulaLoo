84932a0b77d326d092f3866a6a77ad301b611839
Viviane Ketlen <viviane.miranda1510@gmail.com>
2023-09-04 21:44:53 -0400

    Atividade

---
+++ ProjetoEstagiario/src/uniderp/loo/escola/dominio/Funcionario.java
@@ -0,0 +1,29 @@
+package uniderp.loo.escola.dominio;
+
+import java.time.LocalDate;
+
+public class Funcionario extends BasePessoa {
+    private String contaCorrente;
+    private String cracha;
+    
+    public String getContaCorrente() {
+        return contaCorrente;
+    }
+    public void setContaCorrente(String contaCorrente) {
+        this.contaCorrente = contaCorrente;
+    }
+    public String getCracha() {
+        return cracha;
+    }
+    public void setCracha(String cracha) {
+        this.cracha = cracha;
+    }
+    public Funcionario(int codigo, String nome, String email, String telefone, LocalDate dataNascimento, String usuario,
+            String senha, String registro, String contaCorrente, String cracha) {
+        super(codigo, nome, email, telefone, dataNascimento, usuario, senha, registro);
+        this.contaCorrente = contaCorrente;
+        this.cracha = cracha;
+    }
+
+    
+}
